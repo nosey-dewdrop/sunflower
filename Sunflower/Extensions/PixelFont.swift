6b55d327c9f17cf3dd57d097a74aeedb566299e0
nosey-dewdrop <damummyphus@gmail.com>
2026-03-23 14:19:10 +0300

    replace pixel font with system rounded font, cleaner look like focuspomo

--- Sunflower/Extensions/PixelFont.swift
+++ Sunflower/Extensions/PixelFont.swift
@@ -1,26 +1,7 @@
 import SwiftUI
 
-enum PixelFont {
-    static func regular(size: CGFloat) -> Font {
-        .custom("PixelifySans-Regular", size: size)
-    }
-
-    static func bold(size: CGFloat) -> Font {
-        .custom("PixelifySans-Bold", size: size)
-    }
-}
-
-struct PixelFontModifier: ViewModifier {
-    let size: CGFloat
-    let bold: Bool
-
-    func body(content: Content) -> some View {
-        content.font(bold ? PixelFont.bold(size: size) : PixelFont.regular(size: size))
-    }
-}
-
 extension View {
     func pixelFont(size: CGFloat = 16, bold: Bool = false) -> some View {
-        modifier(PixelFontModifier(size: size, bold: bold))
+        self.font(.system(size: size, weight: bold ? .bold : .regular, design: .rounded))
     }
 }
